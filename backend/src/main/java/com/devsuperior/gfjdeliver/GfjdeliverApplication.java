3e802d2f7179498fdb78c895e5c3fa6d53123e6c
gfjgabriel <gfjgabriel@gmail.com>
2021-01-07 13:21:44 -0300

    Initialize project

---
+++ backend/src/main/java/com/devsuperior/gfjdeliver/GfjdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.gfjdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfjdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfjdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/gfjdeliver/GfjdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.gfjdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfjdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
