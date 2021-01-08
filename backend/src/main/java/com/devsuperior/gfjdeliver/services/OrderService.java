a888329cd9fcefa0f2b9896f3de0d51d6d7f62e3
gfjgabriel <gfjgabriel@gmail.com>
2021-01-08 16:02:01 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/gfjdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/gfjdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.gfjdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.gfjdeliver.dto.OrderDTO;
+import com.devsuperior.gfjdeliver.dto.ProductDTO;
 import com.devsuperior.gfjdeliver.entities.Order;
+import com.devsuperior.gfjdeliver.entities.OrderStatus;
+import com.devsuperior.gfjdeliver.entities.Product;
 import com.devsuperior.gfjdeliver.repositories.OrderRepository;
+import com.devsuperior.gfjdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,10 +22,32 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findPendingOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(
+				null, 
+				dto.getAddress(), 
+				dto.getLatitude(), 
+				dto.getLongitude(), 
+				Instant.now(), 
+				OrderStatus.PENDING
+				);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 	
 }
