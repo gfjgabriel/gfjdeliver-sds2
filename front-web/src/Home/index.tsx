7939bbd8ed7e1db3da9d3a4f0af91847a0e82dc4
Gabriel Fortunato Juca <68443737+gfjgabriel@users.noreply.github.com>
2022-10-13 18:11:38 -0300

    barros

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -10,9 +10,9 @@ function Home() {
                 <div className="home-content">
                     <div className="home-actions">
                         <h1 className="home-title">
-                            Faça seu pedido<br /> que entregamos<br /> pra você!!!
+                            Barros delivery!!!
                     </h1>
-                        <h3>Escolha o seu pedido e em poucos minutos<br /> levaremos na sua porta</h3>
+                        <h3>Escolha o seu barro e em poucos minutos<br /> levaremos na sua porta</h3>
                         <Link to="/orders" className="home-btn-order">
                             FAZER PEDIDO
                     </Link>
